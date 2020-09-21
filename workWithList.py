b47c87aaa67cb13d2a2675937008ef8a89e0ee5e
Alexander Schaubert <alexander.schaubert@googlemail.com>
2020-09-21 18:54:23 +0200

    a simple list manipulation

---
+++ workWithList.py
@@ -0,0 +1,7 @@
+#How can you assigne a hello in a liste on 3rd position?
+spam = [2, 4, 6, 8, 10]
+#first how the list looks like before we change it
+print(spam)
+spam[2] = 'hello'
+#how the list looks like after manipulation
+print(spam)
