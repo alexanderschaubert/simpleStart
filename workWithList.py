a6eae5ef97b4f7d229b25c228bca871c79bf5db0
Alexander Schaubert <alexander.schaubert@googlemail.com>
2020-09-21 19:17:18 +0200

    some new line for tupel

--- workWithList.py
+++ workWithList.py
@@ -1,15 +1,33 @@
-#How can you assigne a hello in a liste on 3rd position?
+# How can you assigne a hello in a liste on 3rd position?
 spam = [2, 4, 6, 8, 10]
-#first how the list looks like before we change it
+# first how the list looks like before we change it
 print(spam)
 spam[2] = 'hello'
-#how the list looks like after manipulation
+# how the list looks like after manipulation
 print(spam)
-
 
 """
 Simple examples with list
 """
 spam = ['a', 'b', 'c', 'd']
-#'3'*2 is equal to 33. In next step 33/11 is 3. So spam on 3rd position is d
-print(spam[int(int('3'*2)/11)])
+# '3'*2 is equal to 33. In next step 33/11 is 3. So spam on 3rd position is d
+print(spam[int(int('3' * 2) / 11)])
+
+# In the list from first element till second.
+# Result is a,b
+print(spam[:2])
+
+bacon = [3.14, 'cat', 11, 'cat', True, 'cat']
+print(bacon.index('cat'))
+bacon.append(99)
+print(bacon)
+bacon.remove('cat')
+print(bacon)
+
+"""
+Tupel
+"""
+tupleSimple = (42)
+#Convert a list to tuple with tuple
+fromListTuple = tuple(bacon)
+print(fromListTuple.count('cat'))
