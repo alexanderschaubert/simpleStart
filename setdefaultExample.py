31a6a0289d538a471a69a8967e554ebf95330fa1
Alexander Schaubert <alexander.schaubert@googlemail.com>
2020-09-22 20:12:28 +0200

    Added some aditional informations in comments

--- setdefaultExample.py
+++ setdefaultExample.py
@@ -1,6 +1,8 @@
 """
 how dictionary looks like
 dictionary{key:value, key:value}
+The target for this code is to count how
+often some letters appears in a sentence
 """
 import pprint
 
@@ -10,6 +12,7 @@ count = {}
 
 for character in message:
  #this line create a key and a default value. For example first catch get 0
+ #character, 0 --> I:0, t:0...
  count.setdefault(character, 0)
  #The value of a key increase +1. It means a key in our example a letter
  #was found once again increase the counter +1 for key key
