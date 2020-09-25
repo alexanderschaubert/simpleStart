48d9d46274b584444bc4dc578d9456b49874cdf8
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-25 19:04:53 +0200

    same name for class parameters

--- moveaFile.py
+++ moveaFile.py
@@ -1,27 +1,16 @@
 import os
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import shutil
 
-class Handler(FileSystemEventHandler):
-    def on_modified(self, event):
-        for filename in os.listdir(folder_track):
-            file = folder_track + "\\" +filename
-            new_path = folder_dest + "\\" +filename
-            os.rename(file, new_path)
+folder_track = "C:\\Users\\alexander.schaubert\\Desktop\\test"
+folder_dest = "C:\\Users\\alexander.schaubert\\Desktop\\test1"
 
-folder_track = "C:\\Users\\alexander.schaubert\\Downloads"
-folder_dest = "C:\\Users\\alexander.schaubert\\Desktop\\test"
+class SimpleClass():
+    def executeMethode(self, folder_track, folder_dest):
+     for filename in os.listdir(folder_track):
+      file = folder_track + "\\" +filename
+      new_path = folder_dest + "\\" +filename
+      shutil.move(file, new_path)
 
-handle = Handler()
-observer = Observer()
-observer.schedule(handle, folder_track, recursive=True)
-observer.start()
 
-try:
- while(True):
-     time.sleep(3)
-except KeyboardInterrupt:
-    observer.stop()
-
-observer.join()
+simplesClass = SimpleClass()
+simplesClass.executeMethode(folder_track, folder_dest)
