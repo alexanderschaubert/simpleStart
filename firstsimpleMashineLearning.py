ebd2a5096df200d9989f6c9daa7ac3a067fffb1b
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-26 21:31:49 +0200

    final state

--- firstsimpleMashineLearning.py
+++ firstsimpleMashineLearning.py
@@ -27,3 +27,31 @@ def resize_image(img, label):
 
 train_resized = train[0].map(resize_image)
 train_batches = train_resized.shuffle(1000).batch(16)
+
+base_layers = tf.keras.applications.MobileNetV2(input_shape=(SIZE, SIZE, 3), include_top=False)
+base_layers.trainable = False
+
+model = tf.keras.Sequential([
+                             base_layers,
+                             GlobalAveragePooling2D(),
+                             Dropout(0.2),
+                             Dense(1)
+])
+model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+
+model.fit(train_batches, epochs=1)
+
+model.fit(train_batches, epochs=2)
+
+files.upload()
+
+for i in range(4):
+  img = load_img(f'cat{i+2}.jpg')
+  img_array = img_to_array(img)
+  img_resized, _ = resize_image(img_array, _)
+  img_expended = np.expand_dims(img_resized, axis=0)
+  prediction = model.predict(img_expended)[0][0]
+  pred_label = 'cat' if prediction < 0.5 else 'dog'
+  plt.figure()
+  plt.imshow(img)
+  plt.title(f'{pred_label} {prediction}')
