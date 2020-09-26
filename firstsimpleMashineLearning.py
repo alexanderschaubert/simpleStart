9c0db4250026247315dc3a2cdd09e115e1b422d0
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-26 21:01:06 +0200

    a simple example to check cat or dog

---
+++ firstsimpleMashineLearning.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+import matplotlib.pyplot as plt
+from google.colab import files
