f4b344e0e12282a58daf38d996a6317f5c9dd5eb
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-27 22:39:11 +0200

    Simple count for a dictionary

---
+++ wovels.py
@@ -0,0 +1,12 @@
+wovels = ['a', 'e', 'e', 'o', 'u', 'i']
+found = {}
+
+#it's a very simple way how you can count wovels
+#in dictionary
+found['a']=0
+found['a'] = found['a']+1
+found['o']=0
+found['o']=found['o']+1
+print(found)
+
+#next time something elegant solution
