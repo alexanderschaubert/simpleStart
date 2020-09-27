c3640a9c3f347c72a1a7f81b72e324d22e4d9f3a
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-27 20:50:32 +0200

    comment without special informations

--- simpleGameWithDictionary.py
+++ simpleGameWithDictionary.py
@@ -9,4 +9,10 @@ def display_inventory(inventory):
      item_total+= v
  print("Total number of items: " + str(item_total))
 
+def addToInventory(inventory, addedItems):
+
+inv = {'gold coin': 42,  'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'rubby']
+inv = addToInventory(inv, dragonLoot)
+display_inventory(inv)
 display_inventory(stuff)
