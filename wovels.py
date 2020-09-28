b24e6df0d73c6edcf1de1016a056d65032c43a73
Alexander Schaubert <alexander.schaubert@gmail.com>
2020-09-28 16:37:27 +0200

    add .setdefault methode and how to get the key and value from a dictionary

--- wovels.py
+++ wovels.py
@@ -4,9 +4,18 @@ found = {}
 #it's a very simple way how you can count wovels
 #in dictionary
 found['a']=0
-found['a'] = found['a']+1
+found['a'] = found['a']+2
 found['o']=0
 found['o']=found['o']+1
 print(found)
 
 #next time something elegant solution
+found.setdefault('b',0)
+found.setdefault('a',2)
+print(found)
+
+# how to get key and value
+# for k in found --> key
+# found[k] --> is value
+for k in found:
+    print(k, 'was found ', found[k])
